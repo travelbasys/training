75877b64b99dbd1c2a51088d3b4af36b8ea56b55
MarcelHaut <marcel.haut@travelbasys.de>
2012-09-12 09:15:02 +0200

    Neu: Import/Export von csv zu txt

--- src/de/travelbasys/training2/Export.java
+++ src/de/travelbasys/training2/Export.java
@@ -17,23 +17,29 @@ public class Export {
 	public static void run(String[] args) {
 		try {
 
+			// Wird zum lesen aus der txt Datei benötigt.
 			FileReader fr = new FileReader(FILE);
 			BufferedReader br = new BufferedReader(fr);
 
+			// wird zum schreiben in die txt Datei benötigt.
 			FileWriter fw = new FileWriter(FILE, true);
 			PrintWriter pw = new PrintWriter(fw);
-
+			
+			// wird zum schreiben in die csv Datei benötigt.
 			FileWriter fwcsv = new FileWriter(CSV);
 			PrintWriter pwcsv = new PrintWriter(fwcsv);
 
 			String s;
 			User tempUser;
-
+			// gibt die vorhandenen Daten aus und formatiert sie für die csv
+			// Datei
 			while ((s = br.readLine()) != null) {
+				System.out.println(s);
 				tempUser = User.parse(s);
 				pwcsv.println(tempUser.toCSV());
 			}
-System.out.println(bundle.getString("ExportOK"));
+			// Gibt bei erfolgreichem Import eine bestätigung aus
+			System.out.println(bundle.getString("ExportOK"));
 			fr.close();
 			pw.close();
 			pwcsv.close();
--- src/de/travelbasys/training2/Import.java
+++ src/de/travelbasys/training2/Import.java
@@ -16,21 +16,22 @@ public class Import {
 
 	public static void run(String[] args) {
 		try {
-
+			// Wird zum lesen aus der csv Datei benötigt.
 			FileReader fr = new FileReader(CSV);
 			BufferedReader br = new BufferedReader(fr);
-
+			// wird zum schreiben in die txt Datei benötigt.
 			FileWriter fw = new FileWriter(FILE);
 			PrintWriter pw = new PrintWriter(fw);
 
 			String s;
 			User tempUser;
-
+			// gibt die vorhandenen Daten aus und formatiert sie für die txt Datei
 			while ((s = br.readLine()) != null) {
 				System.out.println(s);
 				tempUser = User.parseCSV(s);
 				pw.println(tempUser.toString());
 			}
+			// Gibt bei erfolgreichem Import eine bestätigung aus
 			System.out.println(bundle.getString("ImportOK"));
 			fr.close();
 			pw.close();
