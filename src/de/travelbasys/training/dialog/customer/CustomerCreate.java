0ec1ebf2c5a8d7383a65506c420dff4d1110cab8
Hubert Kauker <hubert.kauker@travelbasys.de>
2012-10-04 09:21:20 +0200

    Kommentare in Customer Klasse entfernt

--- src/de/travelbasys/training/dialog/customer/CustomerCreate.java
+++ src/de/travelbasys/training/dialog/customer/CustomerCreate.java
@@ -9,36 +9,14 @@ import de.travelbasys.training.util.AppContext;
 import de.travelbasys.training.util.Datum;
 
 /**
- * ist verantwortlich für die Frage nach dem Namen und dem Alter des Benutzters
- * und gibt diese nach System.out aus.
- * 
- * Wird mit einer Message aus der Klasse HelloWorldBusiness initialisiert. Jede
- * Instanz der Klasse wird gezählt; der Instanzenzähler kann abgefragt und von
- * außen verändert werden.
+ * ist verantwortlich für den Dialog mit dem Benutzer, um alle Daten für ein
+ * Customer Objekt abzufragen und das Objekt dann zu erzeugen.
  * 
  * @author tba
- * 
  */
 
 public class CustomerCreate {
 
-	/**
-	 * 
-	 * @param userid
-	 *            Die eindeutige Identifikationsnummer des Benutzers.
-	 * @param firstname
-	 *            Der eingegebene Vorname
-	 * @param lastname
-	 *            Der eingegebene Nachname des Benutzers (Leer initialisiert).
-	 * @param age
-	 *            Das eingegebene Alter des Benutzers.
-	 * @param adress
-	 *            Die eingegebene Adresse des Benutzers.
-	 * @param postalcode
-	 *            Die eingegebene Postleitzahl des Benutzers.
-	 * @param email
-	 *            Die eingegebene e-Mail-Adresse des Benutzers.
-	 **/
 	private String lastname = "";
 	private int userid = 0;
 	private String firstname = "";
@@ -48,21 +26,6 @@ public class CustomerCreate {
 	private String message;
 	private int age = 0;
 
-	/**
-	 * Das eingegebene Alter des Benutzers. Wird mit 0 (=Null) initialisiert.
-	 */
-
-	/**
-	 * fragt den Benutzer nach seinem Namen und seinem Alter.
-	 * 
-	 * Es ist nicht zulässig, einen leeren Namen einzugeben. Das Alter muss eine
-	 * strikt positive ganze Zahl sein. Die Altersabfrage wird so lange
-	 * wiederholt, bis ein gültiger Wert eingegeben wird, z.B. wenn eine
-	 * negative Zahl oder Null, oder ein nicht-numerischer Textstring eingegeben
-	 * wird.
-	 * 
-	 * Am Ende werden Name und Alter nach System.out ausgegeben.
-	 */
 	public void run() {
 		lastname = Dialog.getString("LastNamePrompt");
 		lastname.trim();
@@ -84,14 +47,6 @@ public class CustomerCreate {
 		}
 	}
 
-	/**
-	 * fragt den Benutzer nach seinem seinem Alter.
-	 * 
-	 * Das Alter muss eine strikt positive ganze Zahl sein. Die Altersabfrage
-	 * wird so lange wiederholt, bis ein gültiger Wert eingegeben wird, z.B.
-	 * wenn eine negative Zahl oder Null, oder ein nicht-numerischer Textstring
-	 * eingegeben wird.
-	 */
 	private void run3() {
 		age = Dialog.getInt("AgePrompt");
 		if (age > 0) {
@@ -139,8 +94,6 @@ public class CustomerCreate {
 		run8();
 	}
 
-	/**
-	 */
 	private void run8() {
 		userid = 0;
 		try {
@@ -157,20 +110,6 @@ public class CustomerCreate {
 			run8();
 		}
 
-	}
-
-	/**
-	 * initialisiert die aktuelle Instanz, indem das als Parameter übergebene
-	 * Business Objekt nach der gültigen Message gefragt wird und dieser Wert
-	 * intern aufbewahrt wird.
-	 * 
-	 * Außerdem wird der interne Instanzenzähler um Eins erhöht.
-	 * 
-	 * @param b
-	 *            ein Business Objekt.
-	 */
-	public void init(Datum b) {
-		message = b.getMessage();
 	}
 
 }
