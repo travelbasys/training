94c05ab424b0c2d668e1915ecb9acce959358142
tba <tba@travelbasys.de>
2012-11-28 15:19:50 +0100

    Kommentare

--- src/experiment/javafx/travelbasys/Main.java
+++ src/experiment/javafx/travelbasys/Main.java
@@ -9,35 +9,41 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	public static void main(String[] args) {
+		// Starte die grafische Oberfläche
 		launch(args);
 	}
 
+	// Startmethode der GUI.
 	@Override
 	public void start(final Stage primaryStage) {
 
 		// Titel festlegen
 		primaryStage.setTitle("Travelbasys Customer Manager");
 
+		// Erstelle einzelne Menüs
 		final Menu menu1 = new Menu("File");
 		final Menu menu2 = new Menu("Edit");
 		final Menu menu3 = new Menu("Options");
 		final Menu menu4 = new Menu("Help");
 
-		// MenuBar
+		// MenuBar erstellen
 		MenuBar menuBar = new MenuBar();
+
+		// Der MenuBar eine Collection von Menu-Objekten hinzufügen
 		menuBar.getMenus().addAll(menu1, menu2, menu3, menu4);
 
 		// Hauptpanel erstellen
 		Pane root = new Pane();
 
-		// Dem Hauptpanel alle MenuBar-Objekte hinzufügen
+		// Dem Hauptpanel die MenuBar hinzufügen
 		root.getChildren().addAll(menuBar);
 
 		// Größe des Hauptfensters festlegen
 		primaryStage.setScene(new Scene(root, 640, 480));
-		
-		//Größe der MenuBar auf die Länge des Hauptfensters anpassen
-		menuBar.prefWidthProperty().setValue(primaryStage.getScene().getWidth());
+
+		// Größe der MenuBar auf die Länge des Hauptfensters anpassen
+		menuBar.prefWidthProperty()
+				.setValue(primaryStage.getScene().getWidth());
 
 		// Fenster anzeigen
 		primaryStage.show();
