f9abdb1fa0b1e0f342e6f5faa792647250472317
tba <tba@travelbasys.de>
2012-12-03 09:42:16 +0100

    Testclass & Settings Reset

---
+++ src/experiment/javafx/travelbasys/Main2.java
@@ -0,0 +1,37 @@
+package experiment.javafx.travelbasys;
+
+import javafx.application.Application;
+import javafx.scene.control.Dialogs;
+import javafx.stage.Stage;
+
+/**
+ * Hat die Aufgabe eine grafische Oberfläche bereitzustellen, welche Zugriff auf
+ * Datenbankfunktionen bietet, um ein Customer-Objekt zu verwalten.
+ * 
+ * @author tba
+ * 
+ */
+
+public class Main2 extends Application {
+
+	/**
+	 * Dieses Objekt repräsentiert ein BorderPane Objekt (Panel), welches in der
+	 * aktuellen Implementierung von der Applikation beim Start gesetzt wird &
+	 * durch Menüpunkte verändert werden kann. TODO: Implementierung verändern,
+	 * auslagern in andere Methoden & Klassen. Zurzeit ein großer Haufen
+	 * zusammengewürfelter Aktionen. Absolut nicht zufriedenstellend.
+	 * 
+	 * @pane Ein BorderPane Objekt.
+	 */
+
+	public static void main(String[] args) {
+		// Starte die grafische Oberfläche
+		launch(args);
+	}
+
+	// Startmethode der GUI.
+	@Override
+	public void start(final Stage primaryStage) {
+		Dialogs.showInformationDialog(primaryStage, "Test");
+	}
+}
